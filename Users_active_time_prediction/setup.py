5e67fe2260e8e874b2a56f97ff14460d0ac630f1
Nadiia <nadiapoliakovska@gmail.com>
2023-04-12 11:09:05 -0700

    Add files via upload

---
+++ Users_active_time_prediction/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting of nutime using the app in hours by different users',
+    author='Nadiia Poliakovska',
+    license='',
+)
